9daec5f355eb9202da317b5aef7e064cfeaee106
on <dddonnon@gmail.com>
2021-11-28 15:42:33 +0900

    Add - 기본 컴포넌트 형태 추가

---
+++ client/components/todo/Todo.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Todo: React.FC = () => {
+  return <div>Todo</div>;
+};
+
+export default Todo;
---
+++ client/components/todo/Todos.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Todos: React.FC = () => {
+  return <div>Todos</div>;
+};
+
+export default Todos;
