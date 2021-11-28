9b989a2b5d24a977e0bf51726d5091c096206829
on <dddonnon@gmail.com>
2021-11-28 16:01:46 +0900

    Add - BumaButton 예제 추가

--- client/components/common/Button.tsx
+++ client/components/common/Button.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import 'bulma/css/bulma.min.css'
+import {Button as BulmaButton} from 'react-bulma-components'
 
 const Button: React.FC = () => {
-  return <div>Button</div>
+  return <BulmaButton color="primary">Button</BulmaButton>
 }
 
 export default Button
