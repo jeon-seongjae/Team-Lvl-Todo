842d84ee2130e1bf330146c477051b55d59419c7
yourcarrot0214 <stylenbs@gmail.com>
2021-11-30 21:21:31 +0900

    create ButtonComponent

---
+++ client/components/common/ButtonComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import 'bulma/css/bulma.min.css'
+import {Button as BulmaButton} from 'react-bulma-components'
+
+const ButtonComponent: React.FC = () => {
+  return <BulmaButton color="primary">Button</BulmaButton>
+}
+
+export default ButtonComponent
