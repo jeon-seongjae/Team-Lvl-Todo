f737cb6188a7c686efc76feeced1911f6e9fdce1
jeon-seongjae <tjdwoek62@google.com>
2021-12-02 17:39:47 +0900

    Add #30 - edit: api내용추가및유효성검증

--- server/src/todo/dto/create-todo.dto.ts
+++ server/src/todo/dto/create-todo.dto.ts
@@ -1,1 +1,22 @@
-export class CreateTodoDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTodoDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: '오늘 할 일',
+    description: '제목',
+    required: true,
+  })
+  public title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: '코딩',
+    description: '내용',
+    required: true,
+  })
+  public content: string;
+}
--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,1 +1,30 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @ApiProperty({
+    example: 'test@gmail.com',
+    description: '이메일',
+    required: true,
+  })
+  public email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 'jeon',
+    description: '닉네임',
+    required: true,
+  })
+  public nickname: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 'test',
+    description: '비밀번호',
+    required: true,
+  })
+  public password: string;
+}
