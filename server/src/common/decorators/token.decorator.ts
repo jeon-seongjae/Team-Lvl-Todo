0b93099e13fb6e95b50751bd19853b8d976f1e5d
jeon-seongjae <tjdwoek62@google.com>
2021-12-03 23:55:40 +0900

    Add #34 - edit: 회원가입기능구현현

---
+++ server/src/common/decorators/token.decorator.ts
@@ -0,0 +1,8 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const User = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const response = ctx.switchToHttp().getResponse();
+    return response.locals.jwt;
+  },
+);
