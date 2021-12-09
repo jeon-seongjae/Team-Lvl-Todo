43efe42e4968ba87e9b237eb3bdd8e9ef0b8d9d9
on <dddonnon@gmail.com>
2021-12-09 23:39:57 +0900

    feat: Todo Api 예제 코드 추가

--- client/api/todo.ts
+++ client/api/todo.ts
@@ -1,17 +1,55 @@
 import axios from 'axios'
 
-export const getTodos = () => {
-  // TODO
+const instance = axios.create({
+  baseURL: 'http://localhost:4000',
+})
+
+interface TodoDto {
+  title: string
+  content: string
+  status: number
 }
 
-export const addTodo = () => {
-  // TODO
-}
+export const TodoApi = {
+  getTodos: async (): Promise<TodoDto[]> => {
+    try {
+      const {data} = await instance.get('/todo')
+      console.log(data)
+      return data
+    } catch (error) {
+      console.error(error)
+    }
 
-export const removeTodo = () => {
-  // TODO
-}
+    return []
+  },
+  addTodo: async (): Promise<TodoDto[]> => {
+    try {
+      const {data} = await instance.post('/todo')
+      return data
+    } catch (error) {
+      console.error(error)
+    }
 
-export const updateTodo = () => {
-  // TODO
+    return []
+  },
+  removeTodo: async (): Promise<TodoDto[]> => {
+    try {
+      const {data} = await instance.delete('/todo/delete')
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+
+    return []
+  },
+  updateTodo: async (): Promise<TodoDto[]> => {
+    try {
+      const {data} = await instance.patch('/todo/update')
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+
+    return []
+  },
 }
